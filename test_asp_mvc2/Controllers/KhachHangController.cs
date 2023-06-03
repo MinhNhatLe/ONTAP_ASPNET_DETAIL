dabf92927d9500f09fb3578c20d9486c3c26e5e6
Nhat Le <symbol057vn@gmail.com>
2023-06-04 01:45:45 +0700

    them sua xoa khach hang, them loai khach hang

--- test_asp_mvc2/Controllers/KhachHangController.cs
+++ test_asp_mvc2/Controllers/KhachHangController.cs
@@ -45,7 +45,7 @@ namespace test_asp_mvc2.Controllers
 
         public ActionResult CapNhat(int id)
         {
-            // tìm đối tượng theo ID để lấy ra
+            // tìm đối tượng theo ID để lấy ra show ra dữ liệu
             KhachHang model = db.KhachHangs.SingleOrDefault(m => m.ID == id);
             // dùng cách này để tìm đối tượng theo ID cũng được
             //KhachHang model1 = db.KhachHangs.Find(id);
@@ -58,13 +58,16 @@ namespace test_asp_mvc2.Controllers
         // đặt tên nó là model 
         public ActionResult CapNhat(KhachHang model, int id)
         {
-            // tìm đối tượng id để lấy ra 
+            // tìm đối tượng id để lấy ra để mà cập nhật
+            // tìm được ID thì nó sẽ show ra tất cả dữ liệu đầy các ô input
             KhachHang ten = db.KhachHangs.SingleOrDefault(m => m.ID == id);
 
-            // gán giá trị
+            //gán giá trị
+            // muốn sửa giá trị nào gán giá trị đó
             ten.TenKhachHang = model.TenKhachHang;
             ten.SoDienThoai = model.SoDienThoai;
             ten.DiaChi = model.DiaChi;
+            ten.idLoaiKhachHang = model.idLoaiKhachHang;
 
             // Lưu lại thay đổi
             db.SaveChanges();
@@ -77,13 +80,42 @@ namespace test_asp_mvc2.Controllers
         public ActionResult Xoa(int id)
         {
             // tìm đối tượng id để lấy ra
+            // cách 1
             KhachHang ten = db.KhachHangs.SingleOrDefault(m => m.ID == id);
+
+            // cách 2 dùng var khai báo cũng được luôn
+            //var ten = db.KhachHangs.SingleOrDefault(m => m.ID == id);
+
 
             // thực hiện chức năng remove để xóa cái đối tượng id vừa tìm
             db.KhachHangs.Remove(ten);
 
             // Lưu lại thay đổi
             db.SaveChanges();
+            return RedirectToAction("DanhSach");
+        }
+
+        public ActionResult XoaKhachHangTheoNhom(int idLoaiKhachHang)
+        {
+            //1: Xóa dữ liệu các bảng cần xóa: Loại khách hàng
+            var modelLoaiKhachHang = db.LoaiKhachHangs.Find(idLoaiKhachHang);
+            db.LoaiKhachHangs.Remove(modelLoaiKhachHang);
+            db.SaveChanges();
+
+            //2: Xóa bảng con liên quan: Khách hàng: xóa luôn các khách hàng thuộc nhóm đó
+            var danhSachKhachHangThuocNhom = db.KhachHangs.Where(m => m.idLoaiKhachHang == idLoaiKhachHang).ToList();
+            db.KhachHangs.RemoveRange(danhSachKhachHangThuocNhom);
+            db.SaveChanges();
+
+            // xóa lẻ
+            foreach(var khachHang in danhSachKhachHangThuocNhom)
+            {
+                if(khachHang.DiaChi == "TN")
+                {
+                    db.KhachHangs.Remove(khachHang);
+                }
+            }
+
             return RedirectToAction("DanhSach");
         }
     }
